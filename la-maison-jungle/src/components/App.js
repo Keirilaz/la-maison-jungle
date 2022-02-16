e906b3586c0531e79d82a6b1b596a97c226dbabf
Kévin <91655037+Keirilaz@users.noreply.github.com>
2022-02-16 15:52:42 +0100

    update

--- la-maison-jungle/src/components/App.js
+++ la-maison-jungle/src/components/App.js
@@ -1,12 +1,19 @@
-import './App.css';
-import Banner from './Banner';
-import Cart from './Cart';
+import Banner from './Banner'
+import logo from '../assets/logo.png'
+import Footer from './Footer'
+import ShoppingList from './ShoppingList';
 
 function App() {
   return (
     <div>
-      <Banner />
-      <Cart />
+      <Banner>
+        <img src= {logo} alt='La maison jungle' className='lmj-logo' />
+        <h1 className='lmj-tittle'>La maison jungle</h1>
+      </Banner>
+      {/* <Cart /> */}
+      <ShoppingList />
+      <Footer />
+      
     </div>
   )
 
