76d3d26ebdd010881c1a22aeec5222de11727eac
Kévin <91655037+Keirilaz@users.noreply.github.com>
2022-02-17 12:01:05 +0100

    nouveau panier et nouvelle cover

--- la-maison-jungle/src/components/App.js
+++ la-maison-jungle/src/components/App.js
@@ -1,22 +1,24 @@
 import Banner from './Banner'
 import logo from '../assets/logo.png'
 import Footer from './Footer'
-import ShoppingList from './ShoppingList';
+import Cart from './Cart'
+import ShoppingList from './ShoppingList'
+import '../styles/Layout.css'
 
 function App() {
-  return (
-    <div>
-      <Banner>
-        <img src= {logo} alt='La maison jungle' className='lmj-logo' />
-        <h1 className='lmj-tittle'>La maison jungle</h1>
-      </Banner>
-      {/* <Cart /> */}
-      <ShoppingList />
-      <Footer />
-      
-    </div>
-  )
-
+	return (
+		<div>
+			<Banner>
+				<img src={logo} alt='La maison jungle' className='lmj-logo' />
+				<h1 className='lmj-title'>La maison jungle</h1>
+			</Banner>
+			<div className='lmj-layout-inner'>
+				<Cart />
+				<ShoppingList />
+			</div>
+			<Footer />
+		</div>
+	)
 }
 
-export default App;
+export default App
--- la-maison-jungle/src/components/PlantItem.js
+++ la-maison-jungle/src/components/PlantItem.js
@@ -2,20 +2,20 @@ import CareScale from './CareScale'
 import '../styles/PlantItem.css'
 
 function handleClick(plantName) {
-    alert(`Vous voulez acheter 1 ${plantName} ? Très bon choix`)
+	alert(`Vous voulez acheter 1 ${plantName}? Très bon choix 🌱✨`)
 }
 
-function PlantItem({ cover,name,water,light}){
-    return (
-        <li className='lmj-plant-item' onClick={() => handleClick}>
-            <img className='lmh-plant-item-cover' src={cover} alt={`${name} cover`}/>
-            {name}
-            <div>
-                <CareScale careType='water' scaleValue={water} />
+function PlantItem({ cover, name, water, light }) {
+	return (
+		<li className='lmj-plant-item' onClick={() => handleClick}>
+			<img className='lmj-plant-item-cover' src={cover} alt={`${name} cover`} />
+			{name}
+			<div>
+				<CareScale careType='water' scaleValue={water} />
 				<CareScale careType='light' scaleValue={light} />
-            </div>
-        </li>
-    )
+			</div>
+		</li>
+	)
 }
 
 export default PlantItem
